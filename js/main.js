7761383bd9726f425aea874d839c9b972de18dc5
serg6600 <77173743+serg6600@users.noreply.github.com>
2021-03-25 17:50:32 +0300

    second day

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
-const modalClose = document.querySelector('.modal-close');
 
 const openModal = function() {
 	modalCart.classList.add('show');
@@ -24,7 +23,13 @@ const closeModal = function() {
 
 buttonCart.addEventListener('click', openModal);
 
-modalClose.addEventListener('click', closeModal);
+modalCart.addEventListener('click', function(event){
+	const target = event.target;
+	console.log(target);
+	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) {
+		closeModal()
+	}
+})
 
 //scroll smooth
 
@@ -40,3 +45,73 @@ for (let i = 0; i<scrollLinks.length; i++){
 		})
 	});
 }
+
+//goods
+
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function(){
+	const result = await fetch('db/db.json');
+	if (!result.ok){
+		throw 'ErrorRrrRr! ' + result.status
+	}
+	return result.json();
+}
+
+const createCard = function(objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+
+	card.innerHTML=`
+	<div class="goods-card">
+		${objCard.label ? 
+			`<span class="label">${objCard.label}</span>` :
+			''
+			}
+		
+		<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+		<h3 class="goods-title">${objCard.name}</h3>
+		<p class="goods-description">${objCard.description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+		<span class="button-price">$${objCard.price}</span>
+	</button>
+	</div>
+	`;
+	return card;
+
+};
+
+const renderCards = function(data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+
+	document.body.classList.add('show-goods')
+};
+
+more.addEventListener('click', function(event){
+	event.preventDefault();
+	getGoods().then(renderCards);
+})
+
+const filterCards = function(field, value){
+	getGoods()
+		.then(function(data){
+			const filteredGoods = data.filter(function(good){
+				return good[field] === value 
+			})
+		return filteredGoods;
+	})
+	.then(renderCards);
+}
+
+navigationLink.forEach(function(link){
+	link.addEventListener('click', function(event){
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field, value)
+	})
+})
